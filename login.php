3acd41cbe403acf8432dfa1b4b84fa613d056f8e
IkraMahfoud <103376819+IkramMahfoud@users.noreply.github.com>
2022-12-22 21:26:42 +0100

    Front-end

---
+++ login.php
@@ -0,0 +1,29 @@
+<!-- include header -->
+
+
+
+<!-- include Tailwind -->
+<script src="https://cdn.tailwindcss.com"></script>
+
+
+  <div class="relative flex min-h-screen text-gray-800 antialiased flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12" style="background-color: #e3f2fd;">
+    <div class="relative py-3 sm:w-96 mx-auto text-center">
+      <span class="text-2xl font-light ">Log in to your account</span>
+      <div class="mt-4 bg-white shadow-md rounded-lg text-left">
+        <div class="h-2 bg-purple-400 rounded-t-md"></div>
+        <div class="px-8 py-6 ">
+          <label class="block font-semibold"> Email </label>
+          <input type="text" placeholder="Email" class="border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-indigo-500 focus:ring-1 rounded-md">
+          <label class="block mt-3 font-semibold"> Password </label>
+          <input type="password" placeholder="Password" class="border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-indigo-500 focus:ring-1 rounded-md">
+          <div class="flex justify-between items-baseline">
+            <button type="submit" class="mt-4 bg-purple-500 text-white py-2 px-6 rounded-md hover:bg-purple-600 ">Login</button>
+            <a href="#" class="text-sm hover:underline">Forgot password?</a>
+          </div>
+        </div>
+      </div>
+    </div>
+ 
+
+
+  <!-- include footer -->
